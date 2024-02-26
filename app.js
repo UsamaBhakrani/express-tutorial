0b09944a7c9d9797c7c61886620e2c57a8335c08
Mohammed Usama <usamabhakrani@gmail.com>
2024-02-26 17:41:27 +0500

    CRUD

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get("/api/people", (req, res) =>
 // Javascript Post
 app.post("/api/people", (req, res) => {
   if (!req.body.name) {
-    return res.status(404).json("Please Provide User Name");
+    return res.status(404).send("Please Provide User Name");
   }
   res.status(200).json({ name: req.body.name, id: people.length + 1 });
 });
@@ -31,6 +31,27 @@ app.post("/login", (req, res) => {
     return res.status(404).send("Please Provide User Name");
   }
   res.status(200).json({ name: req.body.name, id: people.length + 1 });
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const newPerson = people.find((p) => p.id === parseInt(id));
+
+  if (!newPerson) return res.status(404).send("Enter ID");
+
+  newPerson.name = name;
+  res.json(newPerson);
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const newPerson = people.find((p) => p.id === parseInt(req.params.id));
+
+  if (!newPerson) return res.status(404).send("Enter Proper ID to Delete");
+  
+  const newList = people.filter((p) => p.id !== parseInt(req.params.id));
+  
+  res.status(200).json(newList);
 });
 
 app.listen(port, () => console.log("server running on port", port));
